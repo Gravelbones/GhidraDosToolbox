a1ffdafde2f48d2419adbca446344cdb2f4db017
Gravelbones <bojesen67@gmail.com>
2023-01-17 19:00:21 +0100

    Updated ResolveDosSyscalls script

--- ghidra_scripts/ResolveDosSyscalls.java
+++ ghidra_scripts/ResolveDosSyscalls.java
@@ -1,5 +1,5 @@
 /* ###
- * IP: Morten Rønne
+ * IP: Morten RÃ¸nne
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
